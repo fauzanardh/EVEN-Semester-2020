4b3ba3b10dca3215f290f8064e267d100f62a02e
FauzanArdh <fauzanardh91@gmail.com>
2020-02-19 10:29:26 +0700

    git init

---
+++ 2020-02-19/np_practice.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# 1D numpy array
+A = np.array([1, 2, 3, 4])
+
+# 2D numpy array
+B = np.array([[1, 2, 3], [4, 5, 6]])
+# 2D zero-fill numpy array
+C = np.zeros((2, 3))
+# 2D one-fill numpy array
+D = np.ones((2, 3))
+# 2D diagonal numpy array
+E = np.diag([1, 2])
